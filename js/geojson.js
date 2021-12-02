b628ce22578239e89583f4178ffdfbfd140c2ce1
kuangkeng <kuangkeng@gmail.com>
2021-12-03 02:03:06 +0800

    first commit

---
+++ js/geojson.js
@@ -0,0 +1,169 @@
+const assaultGEOJson = {
+  type: 'FeatureCollection',
+  features: [{
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70921564102173, 3.1677582028946785]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70796036720276, 3.1680474388043045]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70726299285887, 3.167833189990129]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70607209205627, 3.1680367263646474]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.7053747177124, 3.1678117651062596]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.7044734954834, 3.1681010010009243]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70390486717224, 3.1678117651062596]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70278906822205, 3.167833189990129]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70010685920715, 3.1679831641647067]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70097589492798, 3.1677796277796415]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.6998815536499, 3.167543954020619]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.70205682516098, 3.1680286920348406]
+      }
+    }
+  ]
+};
+
+const tarGEOJson = {
+  type: 'FeatureCollection',
+  features: [{
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.69839292764664, 3.1657817553497947]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.69829368591309, 3.1659611890901616]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.69828563928604, 3.16544966894234]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.69831782579422, 3.165066698195148]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.6981890797615, 3.1646676585946847]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [101.6982614994049, 3.1643248593521283]
+      }
+    },
+    // {
+    //   type: 'Feature',
+    //   properties: {},
+    //   geometry: {
+    //     type: 'Point',
+    //     coordinates: [101.69813543558121, 3.1638883257775485]
+    //   }
+    // },
+    // {
+    //   type: 'Feature',
+    //   properties: {},
+    //   geometry: {
+    //     type: 'Point',
+    //     coordinates: [101.69810861349106, 3.163111670497127]
+    //   }
+    // }
+  ]
+};
